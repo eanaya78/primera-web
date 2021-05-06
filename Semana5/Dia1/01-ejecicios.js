8155f0d3f808376253db5cf547c2387e0227ce85
eanaya78 <81399508+eanaya78@users.noreply.github.com>
2021-05-06 18:33:10 -0500

    runs

---
+++ Semana5/Dia1/01-ejecicios.js
@@ -0,0 +1,4 @@
+// 1. Pedir al usuario su nombre y apellido y mostrarlo en un saludo
+let nom = prompt("Ingrese Nombre");
+let ape = prompt("Ingrese Apellido");
+console.log(`Nombre y Apellido: ${nom}${ape}`); 
---
+++ Semana5/Dia1/04-funciones.js
@@ -0,0 +1,10 @@
+function calcularAreaTriangulo(base,altura){
+    // console.log("Estoy ejecutandome dentro de una funcion");
+    let area = base * altura / 2;
+    // console.log(area);
+    return area;
+}
+
+console.log(calcularAreaTriangulo(50,25))
+
+console.log(base)
